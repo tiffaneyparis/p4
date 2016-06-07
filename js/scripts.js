fc72ddf74ade20242b35091795f74ea39caf3d7b
Tiffaney Paris <tiffaneyparis@Tiffaneys-MacBook-Pro.local>
2016-06-07 13:23:27 -0700

    fun with animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,40 @@
 // animate star //
 
-var $star = $('#star');
+var $V = $('#V');
 
 //selector or DOM element, duration, CSS properties
 
-TweenMax.from($star, 5, {
+TweenMax.from($V, 5, {
     
     opacity: 0,
-    x: 180
+    x: 333
     
+ 
     
+});
+
+var $C = $('#C');
+
+//selector or DOM element, duration, CSS properties
+
+TweenMax.from($C, 5, {
+    
+    opacity: 0,
+    y: 333
+    
+ 
+    
+});
+
+var $D = $('#D');
+
+//selector or DOM element, duration, CSS properties
+
+TweenMax.from($D, 5, {
+    
+    opacity: 0,
+    x: 333
+    
+ 
     
 });
