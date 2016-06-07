2623fd52a2567e79ecd2aba51e7ed170346e9d24
Tiffaney Paris <tiffaneyparis@Tiffaneys-MacBook-Pro.local>
2016-06-07 14:28:46 -0700

    working with animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
-// animate star //
+// animate letter //
 
 var $V = $('#V');
-
 //selector or DOM element, duration, CSS properties
 
-TweenMax.from($V, 5, {
+TweenMax.from($V, 2, {
     
     opacity: 0,
     x: 333
-    
- 
+  
     
 });
 
@@ -17,7 +15,7 @@ var $C = $('#C');
 
 //selector or DOM element, duration, CSS properties
 
-TweenMax.from($C, 5, {
+TweenMax.from($C, 3, {
     
     opacity: 0,
     y: 333
@@ -30,11 +28,32 @@ var $D = $('#D');
 
 //selector or DOM element, duration, CSS properties
 
-TweenMax.from($D, 5, {
+TweenMax.from($D, 4, {
     
     opacity: 0,
-    x: 333
+    x: -111
+    
+    
     
  
     
 });
+
+//don't even know 
+  
+ var $vcd_contain = $('.vcd_contain');
+TweenMax.from($vcd_contain, 3, {
+    
+    opacity: 1,
+    x: 450,
+    
+});
+
+//makes letters larger, smaller, changes opacity 
+
+var V = document.getElementById("V");
+var C = document.getElementById("C");
+var D = document.getElementById("D");
+TweenLite.to([V, C, D], 1, {scale:1, opacity:1});
+
+
